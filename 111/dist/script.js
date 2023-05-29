bde80db25ec09c03ecb2196527387a338e6df246
yingqian24 <131647861+yingqian24@users.noreply.github.com>
2023-05-29 15:04:59 +0800

    Add files via upload

--- 111/dist/script.js
+++ 111/dist/script.js
@@ -1,1 +1,9 @@
 document.write("HELLO我是劉映芊 ")
+
+var count = 0;
+    var counterElement = document.getElementById("counter");
+
+    function increment() {
+      count+=2;
+      counterElement.textContent = count;
+    }
